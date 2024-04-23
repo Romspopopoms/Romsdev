4b19948258b4e6561b81f206ebd0de1978034387
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-04-23 12:04:12 +0200

    Update sendMail.js

--- pages/api/sendMail.js
+++ pages/api/sendMail.js
@@ -9,18 +9,18 @@ const sendMail = async (req, res) => {
   const { name, email, message } = req.body;
 
   const transporter = nodemailer.createTransport({
-    host: 'smtp.mail.me.com',
+    host: 'smtp.gmail.com',
     port: 587,
     secure: false, // true pour 465, false pour d'autres ports
     auth: {
-      user: process.env.ICLOUD_EMAIL,
-      pass: process.env.ICLOUD_APP_PASSWORD // Utilisez un mot de passe d'application généré
+      user: `devprosyl@gmail.com`,
+      pass: `W^Nm7j$adM^oNz` // Utilisez un mot de passe d'application généré
     }
   });
 
   const mailOptions = {
     from: `"Formulaire de Contact" <${process.env.ICLOUD_EMAIL}>`,
-    to: process.env.RECIPIENT_EMAIL, // Définissez cette variable dans vos variables d'environnement
+    to: `lantheaume.sylvain@gmail.com@gmail.com`, // Définissez cette variable dans vos variables d'environnement
     subject: `Nouveau message de ${name}`,
     text: message,
     html: `<p><strong>Nom:</strong> ${name} <br><strong>Email:</strong> ${email} <br><strong>Message:</strong> ${message}</p>`
