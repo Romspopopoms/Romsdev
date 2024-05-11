d6a5078293828c9d8c22acbe98214cf8d89f9eb1
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-05-11 18:34:34 +0200

    Description concise des changements apportés

--- components/Layout.js
+++ components/Layout.js
@@ -12,12 +12,15 @@ const sora = Sora({
 });
 
 const Layout = ({ children, pageTitle, pageDescription, imageUrl, pageUrl }) => {
+  const fullTitle = `${pageTitle} | Développement Web | Création de site internet | RomsDev' Portfolio`;
+
   return (
     <div className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}>
       <Head>
-        <title>{pageTitle}</title>
+        <title>{fullTitle}</title>
         <meta name="description" content={pageDescription} />
-        <meta property="og:title" content={pageTitle} />
+        <meta name="keywords" content="développement web, création de site web" />
+        <meta property="og:title" content={fullTitle} />
         <meta property="og:description" content={pageDescription} />
         <meta property="og:image" content={imageUrl} />
         <meta property="og:url" content={pageUrl} />
@@ -25,9 +28,6 @@ const Layout = ({ children, pageTitle, pageDescription, imageUrl, pageUrl }) => 
         <meta name="google-site-verification" content="vW2uqRP0SI2mzRBW4TWIt9et5K4b81k-zzFJyANVKGw" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="canonical" href={pageUrl} />
-        {/* Ajouter les mots-clés dans le méta titre */}
-        <title>{pageTitle}</title>
-        <meta name="keywords" content="développement web, création de site web" />
         {/* Ajouter d'autres métadonnées si nécessaire */}
       </Head>
       <TopLeftImg />
