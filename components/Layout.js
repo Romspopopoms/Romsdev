e002cce5bc2e17813edf75745f4b6391a2e866ef
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-05-16 15:32:47 +0200

    Description concise des changements apportés

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,5 @@
-// components/Layout.js
-import Head from 'next/head';
-import Nav from './Nav';
-import Header from './Header';
-import TopLeftImg from './TopLeftImg';
-import { Sora } from '@next/font/google';
-
-const sora = Sora({
-  subsets: ['latin'],
-  variable: '--font-sora',
-  weights: ['100', '200', '300', '400', '500', '600', '700', '800']
-});
-
 const Layout = ({ children, pageTitle, pageDescription, imageUrl, pageUrl, keywords }) => {
-  // Construction dynamique du titre de la page pour inclure les mots-clés
-  const fullTitle = `${pageTitle} | ${keywords.join(', ')} | RomsDev' Portfolio`;
+  const fullTitle = `${pageTitle} | RomsDev' Portfolio`;
 
   return (
     <div className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}>
@@ -37,5 +23,3 @@ const Layout = ({ children, pageTitle, pageDescription, imageUrl, pageUrl, keywo
     </div>
   );
 };
-
-export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-// pages/_app.js
 import '../styles/globals.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -11,10 +10,9 @@ import { useRouter } from 'next/router';
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
-  // État pour gérer le SEO dynamique
-  const [seo, setSeo] = useState({
-    pageTitle: "Développement Web | Création de site internet | RomsDev' Portfolio",
-    pageDescription: "Je transforme vos idées en solutions digitales. En tant que développeur indépendant, je crée des sites web et applications sur mesure qui répondent précisément à vos besoins.",
+  const defaultSeo = {
+    pageTitle: "Développement Web | RomsDev' Portfolio",
+    pageDescription: "Développeur indépendant créant des sites web et applications sur mesure.",
     imageUrl: "/Romsdev.png",
     pageUrl: 'https://romsdev.fr',
     keywords: [
@@ -27,25 +25,25 @@ function MyApp({ Component, pageProps }) {
       "développement e-commerce",
       "refonte de site internet",
       "freelance développeur web",
-      "création de sites sur mesure La Ciotat",
-      "création de boutiques en ligne Marseille",
-      "maintenance de sites web Bouches-du-Rhône",
-      "agence web Provence-Alpes-Côte d'Azur"
+      "La Ciotat",
+      "Marseille",
+      "Bouches-du-Rhône",
+      "Provence-Alpes-Côte d'Azur"
     ]
-  });
+  };
 
-  // Mettre à jour le SEO si pageProps contient des informations spécifiques à la page
+  const [seo, setSeo] = useState(defaultSeo);
+
   useEffect(() => {
     if (pageProps.seo) {
-      setSeo({...seo, ...pageProps.seo});
+      setSeo({...defaultSeo, ...pageProps.seo});
+    } else {
+      setSeo(defaultSeo);
     }
   }, [pageProps.seo]);
 
-  // Récupérer les mots-clés de la page actuelle à partir de la route
-  const currentPageKeywords = seo.keywords || [];
-
   return (
-    <Layout {...seo} keywords={currentPageKeywords}>
+    <Layout {...seo}>
       <AnimatePresence mode='wait'>
         <div key={router.route} className='h-full'>
           <Transition />
