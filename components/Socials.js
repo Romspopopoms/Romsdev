7d75ff0f167a75e9eced1eaab2751834052f42b8
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-05-18 23:48:52 +0200

    Description concise des changements apportés

--- components/Socials.js
+++ components/Socials.js
@@ -1,20 +1,19 @@
-import Link from "next/link";
 import { RiYoutubeLine, RiInstagramLine, RiFacebookLine } from "react-icons/ri";
 
 const Socials = () => {
   return (
-  <div className="flex items-center gap-x-5 text-lg">
-    <a href="" className="hover:text-accent transition-all duration-300">
-      <RiYoutubeLine />
-    </a>
-    <a href="https://www.instagram.com/dev_roms?igsh=MTdmcGpiNzBsN2VneA%3D%3D&utm_source=qr" className="hover:text-accent  transition-all duration-300">
-      <RiInstagramLine />
-    </a>
-    <a href="https://www.facebook.com/profile.php?id=61559687947882" className="hover:text-accent transition-all duration-300">
-      <RiFacebookLine />
-    </a>
+    <div className="flex items-center gap-x-5 text-lg z-20">
+      <a href="https://www.youtube.com" className="hover:text-accent transition-all duration-300">
+        <RiYoutubeLine />
+      </a>
+      <a href="https://www.instagram.com/dev_roms?igsh=MTdmcGpiNzBsN2VneA%3D%3D&utm_source=qr" className="hover:text-accent transition-all duration-300">
+        <RiInstagramLine />
+      </a>
+      <a href="https://www.facebook.com/profile.php?id=61559687947882" className="hover:text-accent transition-all duration-300">
+        <RiFacebookLine />
+      </a>
     </div>
-  )
+  );
 };
 
 export default Socials;
