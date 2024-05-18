19479b34696da12a73dfa4dc9e15725db69fe181
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-05-18 23:25:39 +0200

    Description concise des changements apportés

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,17 +1,7 @@
 import Slider from "react-slick";
+import { RxCrop, RxPencil2, RxDesktop, RxReader, RxRocket, RxArrowTopRight } from "react-icons/rx";
 
-// icons
-import {
-  RxCrop,
-  RxPencil2,
-  RxDesktop,
-  RxReader,
-  RxRocket,
-  RxArrowTopRight,
-} from "react-icons/rx";
-
-
-// data
+// Data
 export const serviceData = [
   {
     icon: <RxCrop />,
@@ -74,7 +64,7 @@ const ServiceSlider = () => {
     <div className="px-4 mb-24 xl:mb-0 xl:p-0">
       <Slider {...settings}>
         {serviceData.map((item, index) => (
-          <div key={index} className="bg-[rgba(65,47,123,0.15)] h-full rounded-lg px-6 py-8 flex flex-col justify-between gap-y-4 cursor-pointer shadow-lg transition-shadow duration-300 hover:shadow-xl">
+          <div key={index} className="bg-[rgba(65,47,123,0.15)] h-full rounded-lg px-6 py-8 flex flex-col justify-between gap-y-4 cursor-pointer shadow-lg transition-shadow duration-300 hover:shadow-xl service-slide">
             <div className="text-4xl text-accent mb-4">
               {item.icon}
             </div>
@@ -91,6 +81,5 @@ const ServiceSlider = () => {
     </div>
   );
 };
-
 
 export default ServiceSlider;
