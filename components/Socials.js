e0cef94972224f639f72dc1187fe6f1b0616ebb6
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-05-18 22:52:49 +0200

    Description concise des changements apportés

--- components/Socials.js
+++ components/Socials.js
@@ -10,7 +10,7 @@ const Socials = () => {
     <a href="https://www.instagram.com/dev_roms?igsh=MTdmcGpiNzBsN2VneA%3D%3D&utm_source=qr" className="hover:text-accent  transition-all duration-300">
       <RiInstagramLine />
     </a>
-    <a href="https://www.facebook.com/profile.php?id=61559687947882" className="hover:text-accent  transition-all duration-300">
+    <a href="https://www.facebook.com/profile.php?id=61559687947882" className="hover:text-accent transition-all duration-300">
       <RiFacebookLine />
     </a>
     </div>
