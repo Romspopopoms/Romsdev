b6cf75f83faabc0c6faef66d08c427523414ba9a
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-05-27 01:31:48 +0200

    Description concise des changements apportés

--- pages/blog/developpement/Js-framework/index.js
+++ pages/blog/developpement/Js-framework/index.js
@@ -4,7 +4,7 @@ import Head from 'next/head';
 
 const JsFramework = () => {
   return (
-    <div className="min-h-screen h-screen overflow-y-auto hide-scrollbar bg-gray-900" style={{ overflowY: 'auto' }}>
+    <div className="sm:mt-20 mt-20 lg:mt-20 xl:mt-24 h-full w-full xxl:mt-32 hide-scrollbar " style={{ overflowY: 'auto' }}>
       <Head>
         <title>Frameworks JavaScript | RomsDev</title>
         <meta name="description" content="Découvrez les différents Framework JS, React, Vues, Angular et Next.js. Apprenez à coder et passez au niveau supérieur !" />
@@ -12,7 +12,7 @@ const JsFramework = () => {
         <link rel="canonical" href="https://romsdev.fr/blog/developpement/Js-framework" />
       </Head>
       <NavBlog />
-      <div className="flex flex-col items-center justify-center sm:mt-20 mt-28 lg:mt-20 xl:mt-24 h-full w-full xxl:mt-32">
+      <div className="flex flex-col items-center justify-center lg:mt-0 mt-8">
         <h1 className="h1 text-center">Les Frameworks JavaScript<span className="text-accent">.</span></h1>
         <p className="text-center font-bold text-xl xxl:text-2xl xxl:mt-6">
           Découvrez les différents Framework JS, React, Vues, Angular et Next.js. Apprenez à coder, et passez au niveau supérieur !
