7251bdfd98400fa77c1b395ba8fdf4060bcb14d7
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-05-27 15:32:50 +0200

    Description concise des changements apportés

--- components/NavBlog.js
+++ components/NavBlog.js
@@ -33,22 +33,25 @@ const NavBlog = () => {
 
       {/* Menu pour les écrans non-XL */}
       <div ref={menuRef} className={`xl:hidden grid mt-20 h-full w-full fixed left-0 top-0 p-2 bg-gray-900 transition-all duration-1000 ease-in-out transform ${OpenMenu ? 'translate-x-0' : '-translate-x-[100%]'} z-40`}>
-        <div className="flex flex-col items-center mt-12 space-y-8">
+        <div className="flex flex-col items-center mt-16 space-y-8">
           {NavBlogData.map((item, index) => (
-            <Link key={index} href={item.url}>
-              <div className="flex items-center justify-between gap-8 text-white text-xl hover:text-accent transition-all duration-300 w-full relative">
-                <span>{getIconForIndex(index)}</span>
-                <span>{item.title}</span>
-                <span className="text-accent">{getIconForIndex(index)}</span>
-                {index > 1 && (
-                  <div className="absolute left-0 inset-0 flex items-center justify-center bg-gray-800 pointer-events-none z-50">
-                    <div className="text-white h-12 w-full flex items-center justify-center">
-                      🚧 A VENIR 🚧
-                    </div>
+            <div key={index} className="flex items-center justify-center text-white gap-x-8 text-xl hover:text-accent transition-all duration-300 w-full space-y-6 relative">
+              {index <= 1 ? (
+                <Link href={item.url} passHref>
+                  <div className="flex items-center justify-between w-full gap-x-4">
+                    <span>{getIconForIndex(index)}</span>
+                    <span>{item.title}</span>
+                    <span className="text-accent">{getIconForIndex(index)}</span>
                   </div>
-                )}
-              </div>
-            </Link>
+                </Link>
+              ) : (
+                <div className="flex items-center justify-between w-full">
+                  <span>{getIconForIndex(index)}</span>
+                  <span>🚧 A VENIR 🚧</span>
+                  <span className="text-accent">{getIconForIndex(index)}</span>
+                </div>
+              )}
+            </div>
           ))}
         </div>
       </div>
@@ -57,20 +60,23 @@ const NavBlog = () => {
       <div className={`hidden xl:flex flex-col h-full mt-20 w-64 fixed left-0 top-3 bg-gray-800 p-6 z-40 transition-all duration-1000 ease-in-out transform ${OpenMenu ? 'translate-x-0' : '-translate-x-[100%]'}`}>
         <div className='mt-6 space-y-8'>
           {NavBlogData.map((item, index) => (
-            <Link key={index} href={item.url}>
-              <div className="flex items-center justify-center text-white gap-x-8 text-xl hover:text-accent transition-all duration-300 w-full space-y-6 relative">
-                <span className="mt-[11.5%] text-accent">{getIconForIndex(index)}</span>
-                <span className={`${OpenMenu ? 'block' : 'hidden'}`}>{item.title}</span>
-                <span>{getIconForIndex(index)}</span>
-                {index > 1 && (
-                  <div className="absolute left-0 inset-0 flex items-center justify-center bg-gray-500 lg:bg-gray-800 pointer-events-none z-50">
-                    <div className="text-white h-12 w-full flex items-center justify-center">
-                      🚧 A VENIR 🚧
-                    </div>
+            <div key={index} className="flex items-center justify-center text-white gap-x-8 text-xl hover:text-accent transition-all duration-300 w-full space-y-6 relative">
+              {index <= 1 ? (
+                <Link href={item.url} passHref>
+                  <div className="flex items-center justify-between w-full gap-x-4">
+                    <span className=" text-accent">{getIconForIndex(index)}</span>
+                    <span className={`${OpenMenu ? 'block' : 'hidden'}`}>{item.title}</span>
+                    <span>{getIconForIndex(index)}</span>
                   </div>
-                )}
-              </div>
-            </Link>
+                </Link>
+              ) : (
+                <div className="flex items-center justify-between w-full ">
+                  <span className="mt-[11.5%] text-accent">{getIconForIndex(index)}</span>
+                  <span className={`${OpenMenu ? 'block' : 'hidden'}`}>🚧 A VENIR 🚧</span>
+                  <span>{getIconForIndex(index)}</span>
+                </div>
+              )}
+            </div>
           ))}
         </div>
       </div>
@@ -84,24 +90,24 @@ const getIconForIndex = (index) => {
       return <FaHome />;
     case 1:
       return <FaJs />;
-    case 2:
-      return <FaCode />;
-    case 3:
-      return <FaDatabase />;
-    case 4:
-      return <FaShieldAlt />;
-    case 5:
-      return <FaCloud />;
-    case 6:
-      return <FaBrain />;
-    case 7:
-      return <FaPalette />;
-    case 8:
-      return <FaBriefcase />;
-    case 9:
-      return <FaChartLine />;
-    default:
-      return <FaArrowRight />;
+    // case 2:
+    //   return <FaCode />;
+    // case 3:
+    //   return <FaDatabase />;
+    // case 4:
+    //   return <FaShieldAlt />;
+    // case 5:
+    //   return <FaCloud />;
+    // case 6:
+    //   return <FaBrain />;
+    // case 7:
+    //   return <FaPalette />;
+    // case 8:
+    //   return <FaBriefcase />;
+    // case 9:
+    //   return <FaChartLine />;
+    // default:
+    //   return <FaArrowRight />;
   }
 }
 
