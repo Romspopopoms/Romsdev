0846f230f49f4a7eaf66177382e95aa8d1faa934
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-05-27 11:04:41 +0200

    Description concise des changements apportés

--- components/SliderDev2.js
+++ components/SliderDev2.js
@@ -1,110 +1,97 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import { SiNextdotjs } from "react-icons/si";
-import { FaVuejs } from "react-icons/fa";
-import { FaReact } from "react-icons/fa";
-import { FaAngular } from "react-icons/fa";
-import Image from "next/image";
-import bg1 from "../public/bg-1.png";
+import { FaBolt, FaRocket, FaStopwatch, FaTachometerAlt } from "react-icons/fa";
 
 const SliderDev2 = () => {
     const [hovered, setHovered] = useState(false);
-    
+
     return (
         <div className="w-full h-full flex flex-col items-center">
-            <motion.div className="relative lg:hidden min-w-[70%] h-20 "
-            whileHover={{ width: "80%" }}
-            onHoverStart={() => setHovered(true)}
-            onHoverEnd={() => setHovered(false)}>
-                <Image src={bg1} alt="background" className="absolute object-cover w-full h-full rounded-xl" />
-                
+            <motion.div
+                className="relative lg:hidden min-w-[70%] h-20 bg-gray-800 rounded-xl"
+                whileHover={{ width: "80%" }}
+                onHoverStart={() => setHovered(true)}
+                onHoverEnd={() => setHovered(false)}
+            >
                 <motion.div
                     className="z-10 w-full h-full flex flex-col gap-2 items-center justify-center"
                 >
-                    <motion.h2
-                        className="text-xl relative "
-                    >
-                        F R A M E W O R K<span className="text-accent"> .</span> J S
+                    <motion.h2 className="text-xl relative text-white">
+                        P E R F O R M A N C E<span className="text-accent"> .</span>
                     </motion.h2>
-                
+
                     <div className="relative w-full h-12">
                         <motion.div
                             className="absolute top-[10%] left-[12%]"
-                            animate={hovered ? { top: '10%', left: '80%', scale:1.2 } : { top: '10%', left: '12%' }}
-                            transition={{duration: 0.6}}
+                            animate={hovered ? { top: '10%', left: '80%', scale: 1.2 } : { top: '10%', left: '12%' }}
+                            transition={{ duration: 0.6 }}
                         >
-                            <FaVuejs className="size-8 text-green-600" />
+                            <FaBolt className="size-8 text-yellow-500" />
                         </motion.div>
                         <motion.div
                             className="absolute top-[10%] left-[34%]"
-                            animate={hovered ? { top: '10%', left: '58%', scale:1.2 } : { top: '10%', left: '34%' }}
-                            transition={{duration: 0.6}}
+                            animate={hovered ? { top: '10%', left: '58%', scale: 1.2 } : { top: '10%', left: '34%' }}
+                            transition={{ duration: 0.6 }}
                         >
-                            <FaReact className="size-8 text-blue-400" />
+                            <FaRocket className="size-8 text-blue-500" />
                         </motion.div>
                         <motion.div
                             className="absolute top-[10%] left-[58%]"
-                            animate={hovered ? { top: '10%', left: '34%', scale:1.2 } : { top: '10%', left: '58%' }}
-                            transition={{duration: 0.6}}
+                            animate={hovered ? { top: '10%', left: '34%', scale: 1.2 } : { top: '10%', left: '58%' }}
+                            transition={{ duration: 0.6 }}
                         >
-                            <FaAngular className="size-8 text-red-600" />
+                            <FaStopwatch className="size-8 text-red-500" />
                         </motion.div>
                         <motion.div
                             className="absolute top-[10%] left-[80%]"
-                            animate={hovered ? { top: '10%', left: '12%', scale:1.2 } : { top: '10%', left: '80%' }}
-                            transition={{duration: 0.6}}
+                            animate={hovered ? { top: '10%', left: '12%', scale: 1.2 } : { top: '10%', left: '80%' }}
+                            transition={{ duration: 0.6 }}
                         >
-                            <SiNextdotjs className="size-8 text-black" />
+                            <FaTachometerAlt className="size-8 text-green-500" />
                         </motion.div>
                     </div>
                 </motion.div>
             </motion.div>
             {/* Ce div sera visible uniquement sur les écrans LG et XL */}
             <motion.div
-                className="hidden relative lg:flex justify-center items-center min-w-[60%] h-28 group px-8 "
+                className="hidden relative lg:flex justify-center items-center min-w-[60%] h-28 group px-8 bg-gray-800 rounded-xl"
                 whileHover={{ width: "70%" }}
                 onHoverStart={() => setHovered(true)}
                 onHoverEnd={() => setHovered(false)}
             >
-                <Image src={bg1} alt="background" className="absolute object-cover w-full h-full rounded-xl" />
-                <motion.div
-                    className="z-10 flex flex-col gap-2"
-                >
-                    
-                    <motion.h2
-                        className="text-3xl font-bold group-hover:scale-110 transition-transform duration-300 ease-in-out"
-                    >
-                        J A V A S C R I P T / F R A M E W O R K
+                <motion.div className="z-10 flex flex-col gap-2">
+                    <motion.h2 className="text-3xl font-bold text-white group-hover:scale-110 transition-transform duration-300 ease-in-out">
+                        P E R F O R M A N C E
                     </motion.h2>
-                    
+
                     <div className="relative w-full h-12">
                         <motion.div
                             className="absolute top-[25%] left-[12%]"
-                            animate={hovered ? { top: '25%', left: '80%', scale:1.2 } : { top: '25%', left: '12%' }}
-                            transition={{duration: 0.6}}
+                            animate={hovered ? { top: '25%', left: '80%', scale: 1.2 } : { top: '25%', left: '12%' }}
+                            transition={{ duration: 0.6 }}
                         >
-                            <FaVuejs className="size-10 text-green-600" />
+                            <FaBolt className="size-10 text-yellow-500" />
                         </motion.div>
                         <motion.div
                             className="absolute top-[25%] left-[34%]"
-                            animate={hovered ? { top: '25%', left: '58%', scale:1.2 } : { top: '25%', left: '34%' }}
-                            transition={{duration: 0.6}}
+                            animate={hovered ? { top: '25%', left: '58%', scale: 1.2 } : { top: '25%', left: '34%' }}
+                            transition={{ duration: 0.6 }}
                         >
-                            <FaReact className="size-10 text-blue-400" />
+                            <FaRocket className="size-10 text-blue-500" />
                         </motion.div>
                         <motion.div
                             className="absolute top-[25%] left-[58%]"
-                            animate={hovered ? { top: '25%', left: '34%', scale:1.2 } : { top: '25%', left: '58%' }}
-                            transition={{duration: 0.6}}
+                            animate={hovered ? { top: '25%', left: '34%', scale: 1.2 } : { top: '25%', left: '58%' }}
+                            transition={{ duration: 0.6 }}
                         >
-                            <FaAngular className="size-10 text-red-600" />
+                            <FaStopwatch className="size-10 text-red-500" />
                         </motion.div>
                         <motion.div
                             className="absolute top-[25%] left-[80%]"
-                            animate={hovered ? { top: '25%', left: '12%', scale:1.2 } : { top: '25%', left: '80%' }}
-                            transition={{duration: 0.6}}
+                            animate={hovered ? { top: '25%', left: '12%', scale: 1.2 } : { top: '25%', left: '80%' }}
+                            transition={{ duration: 0.6 }}
                         >
-                            <SiNextdotjs className="size-10 text-black" />
+                            <FaTachometerAlt className="size-10 text-green-500" />
                         </motion.div>
                     </div>
                 </motion.div>
--- pages/blog/developpement/Js-framework/index.js
+++ pages/blog/developpement/Js-framework/index.js
@@ -4,7 +4,8 @@ import Head from 'next/head';
 
 const JsFramework = () => {
   return (
-    <div className="sm:mt-20 mt-20 lg:mt-20 xl:mt-24 h-full w-full xxl:mt-32 hide-scrollbar " style={{ overflowY: 'auto' }}>
+    <div className="h-full w-full bg-gray-900 text-white hide-scrollbar" style={{ overflowY: 'auto' }}>
+      <div className="mt-28"></div>
       <Head>
         <title>Frameworks JavaScript | RomsDev</title>
         <meta name="description" content="Découvrez les différents Framework JS, React, Vues, Angular et Next.js. Apprenez à coder et passez au niveau supérieur !" />
@@ -12,14 +13,14 @@ const JsFramework = () => {
         <link rel="canonical" href="https://romsdev.fr/blog/developpement/Js-framework" />
       </Head>
       <NavBlog />
-      <div className="flex flex-col items-center justify-center lg:mt-0 mt-8">
-        <h1 className="h1 text-center">Les Frameworks JavaScript<span className="text-accent">.</span></h1>
+      <div className="flex flex-col items-center justify-center mt- lg:mt-0">
+        <h1 className="text-5xl font-bold mb-4 text-center">Les Frameworks JavaScript<span className="text-accent">.</span></h1>
         <p className="text-center font-bold text-xl xxl:text-2xl xxl:mt-6">
           Découvrez les différents Framework JS, React, Vues, Angular et Next.js. Apprenez à coder, et passez au niveau supérieur !
         </p>
       </div>
       <NavJsDev />
-      <div className="h-28"></div>
+      <div className="h-40"></div>
     </div>
   );
 }
