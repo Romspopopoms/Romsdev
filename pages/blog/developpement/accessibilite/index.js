20a88778a029acb7f4618014f3bf70e0e1aaccd4
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-06-15 14:47:47 +0200

    Description concise des changements apportés

--- pages/blog/developpement/accessibilite/index.js
+++ pages/blog/developpement/accessibilite/index.js
@@ -1,31 +1,9 @@
-// // pages/blog/developpement/accessibilite/index.js
 
-// import React from "react";
-// import Head from "next/head";
-// import NavBlog from "../../../../components/NavBlog";
-// import NavAccess from "../../../../components/NavAccess";
 
-// const Accessibilite = () => {
-//   return (
-//     <div className="h-full w-full bg-gray-900 text-white hide-scrollbar" style={{ overflowY: 'auto' }}>
-//       <div className="mt-28"></div>
-//       <Head>
-//         <title>Accessibilité Web | RomsDev</title>
-//         <meta name="description" content="Découvrez les meilleures pratiques et outils pour rendre vos applications web accessibles à tous." />
-//         <meta name="keywords" content="Accessibilité web, Développement web, WCAG, ARIA" />
-//         <link rel="canonical" href="https://romsdev.fr/blog/developpement/accessibilite" />
-//       </Head>
-//       <NavBlog />
-//       <div className="flex flex-col items-center justify-center mt-12 lg:mt-0 gap-8">
-//         <h1 className="text-5xl font-bold mb-4 text-center">Accessibilité Web<span className="text-accent">.</span></h1>
-//         <p className="text-center font-bold text-xl lg:text-2xl lg:mt-6 mb-8">
-//           Découvrez les meilleures pratiques et outils pour rendre vos applications web accessibles à tous.
-//         </p>
-//       </div>
-//       <NavAccess />
-//       <div className="h-40"></div>
-//     </div>
-//   );
-// };
+const Accessibilite = () => {
+    return (
+        <div>Yo</div>
+    )
+}
 
-// export default Accessibilite;
+export default Accessibilite
